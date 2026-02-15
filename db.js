92d7e2107d0da2978e80cba21eae424571946243
Felix Nyoka <nyokafelix33@gmail.com>
2026-02-16 00:44:09 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,35 +1,30 @@
-/* FENG WORLD TITAN DATABASE V68.0 - 110+ TITLES */
+/* FENG WORLD TITAN DATABASE V68.0 - PLATINUM HEAVY BUILD */
 const FENG_DB = [
-    // --- SERIES (40 TITLES) ---
-    { id: "66732", t: "Stranger Things", y: "2016", g: "SCI-FI", c: "tv", s: 4, e: 8, i: "/x2LSRm21uTEx8P9uS4NiYpuzpYm.jpg" },
-    { id: "1396", t: "Breaking Bad", y: "2008", g: "CRIME", c: "tv", s: 5, e: 13, i: "/ztkUQ09o9h969Re6S9vDUs9GKV3.jpg" },
-    { id: "71446", t: "Money Heist", y: "2017", g: "CRIME", c: "tv", s: 5, e: 10, i: "/reKs8PRmZ9Yv7pT9yW6Wp.jpg" },
-    { id: "1402", t: "The Walking Dead", y: "2010", g: "HORROR", c: "tv", s: 11, e: 24, i: "/n771f8Iq67p6fXv6p5L8fG5K6.jpg" },
-    { id: "76479", t: "The Boys", y: "2019", g: "ACTION", c: "tv", s: 4, e: 8, i: "/mY7Sj9i9QI9In9I9QI9In9I9QI9.jpg" },
-    { id: "93405", t: "Squid Game", y: "2021", g: "DRAMA", c: "tv", s: 1, e: 9, i: "/d99X2M76p2Yv.jpg" },
-    { id: "82856", t: "The Mandalorian", y: "2019", g: "SCI-FI", c: "tv", s: 3, e: 8, i: "/eU1i6eHXp2klv0tKWpjo7uqcM0N.jpg" },
-    { id: "1399", t: "Game of Thrones", y: "2011", g: "DRAMA", c: "tv", s: 8, e: 10, i: "/1XS1oqL_DqXm_0uG0Uj1m.jpg" },
-    { id: "63174", t: "Lucifer", y: "2016", g: "CRIME", c: "tv", s: 6, e: 10, i: "/ekFe9asA9S_0uG0Uj1m.jpg" },
-    { id: "60735", t: "The Flash", y: "2014", g: "SCI-FI", c: "tv", s: 9, e: 13, i: "/rg8N7_DqXm_0uG0Uj1m.jpg" },
-    // ... Continue this pattern for 30 more series entries
-
-    // --- MOVIES (70 TITLES) ---
-    { id: "299534", t: "Avengers Endgame", y: "2019", g: "ACTION", c: "mov", i: "/or06vSqzWBFscv1qCG76uC1fsR6.jpg" },
-    { id: "533535", t: "Deadpool & Wolverine", y: "2024", g: "ACTION", c: "mov", i: "/h6S6Yn6TrCD0t9_693134.jpg" },
-    { id: "155", t: "The Dark Knight", y: "2008", g: "CRIME", c: "mov", i: "/qJ2tW6WMUDp9QEQbvN9pYFWZ9S7.jpg" },
-    { id: "27205", t: "Inception", y: "2010", g: "SCI-FI", c: "mov", i: "/edv5CZvRjS99ayO76Y9Z9D9p2.jpg" },
-    { id: "157336", t: "Interstellar", y: "2014", g: "SCI-FI", c: "mov", i: "/gEU2QniE6E07Qv8uIUglvYvBvU3.jpg" },
-    { id: "475557", t: "Joker", y: "2019", g: "DRAMA", c: "mov", i: "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg" },
-    { id: "1022789", t: "Inside Out 2", y: "2024", g: "DRAMA", c: "mov", i: "/vpnVM9B6NMmAnp6p9S8fG5K6.jpg" },
-    { id: "823464", t: "Godzilla x Kong", y: "2024", g: "ACTION", c: "mov", i: "/tM9Sj9i9QI9In9I9QI9In9I9QI9.jpg" },
-    { id: "693134", t: "Dune: Part Two", y: "2024", g: "SCI-FI", c: "mov", i: "/8b8R8Lbd9uS94IZpBv0srthvS8.jpg" },
-    { id: "653346", t: "Planet of the Apes", y: "2024", g: "SCI-FI", c: "mov", i: "/gKkl37S9vDUs9GKV3.jpg" },
-    { id: "603", t: "The Matrix", y: "1999", g: "SCI-FI", c: "mov", i: "/f89U3_DqXm_0uG0Uj1m.jpg" },
-    { id: "680", t: "Pulp Fiction", y: "1994", g: "CRIME", c: "mov", i: "/d5i_DqXm_0uG0Uj1m.jpg" },
-    { id: "13", t: "Forrest Gump", y: "1994", g: "DRAMA", c: "mov", i: "/ar_DqXm_0uG0Uj1m.jpg" },
-    { id: "278", t: "The Shawshank Redemption", y: "1994", g: "DRAMA", c: "mov", i: "/ly_DqXm_0uG0Uj1m.jpg" }
-    // ... Continue this pattern for 56 more movie entries
+    { 
+        id: "106379", 
+        t: "A Knight of the Seven Kingdoms", 
+        y: "2025", 
+        g: "FANTASY", 
+        c: "tv", 
+        s: 1, e: 6, 
+        i: "/7k7Sj9i9QI9In9I9QI9In9I9QI9.jpg",
+        desc: "A century before Game of Thrones, two unlikely heroes wandered Westeros... a young, naive but courageous knight, Ser Duncan the Tall, and his diminutive squire, Egg.",
+        cast: "Peter Claffey, Dexter Sol Ansell"
+    },
+    { 
+        id: "475557", 
+        t: "Joker", 
+        y: "2019", 
+        g: "DRAMA", 
+        c: "mov", 
+        i: "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg",
+        desc: "During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.",
+        cast: "Joaquin Phoenix, Robert De Niro, Zazie Beetz"
+    },
+    // Add your other 110 titles following this exact structure...
 ];
 
-// Line padding for stability
-for(let i=0; i<1000; i++) { /* Felix Nyoka V68 Integrity */ }
+const TRENDING_SLIDES = [
+    { id: "106379", t: "A Knight of the Seven Kingdoms", img: "https://image.tmdb.org/t/p/original/7k7Sj9i9QI9In9I9QI9In9I9QI9.jpg" },
+    { id: "533535", t: "Deadpool & Wolverine", img: "https://image.tmdb.org/t/p/original/h6S6Yn6TrCD0t9_693134.jpg" }
+];
