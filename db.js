a6953badbf3513ede63c7e9e41b52b69e3047ee8
Felix Nyoka <nyokafelix33@gmail.com>
2026-02-16 00:00:32 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,35 +1,18 @@
-/* FENG WORLD TITAN DATABASE V68.0 
-   Total Entries: 150+
-   Stability Level: Platinum
-*/
-
 const FENG_DB = [
-    // --- SERIES SECTION (50 ENTRIES) ---
-    { id: "66732", t: "Stranger Things", y: "2016", g: "SCI-FI", c: "tv", img: "https://image.tmdb.org/t/p/w300/49WfTJs0vSzMG26Y3otR9C3qOQn.jpg", s: 4, e: 8 },
-    { id: "1396", t: "Breaking Bad", y: "2008", g: "CRIME", c: "tv", img: "https://image.tmdb.org/t/p/w300/ztkUQ09o9h969Re6S9vDUs9GKV3.jpg", s: 5, e: 13 },
-    { id: "1402", t: "The Walking Dead", y: "2010", g: "HORROR", c: "tv", img: "https://image.tmdb.org/t/p/w300/n771f8Iq67p6fXv6p5L8fG5K6.jpg", s: 11, e: 24 },
-    { id: "71446", t: "Money Heist", y: "2017", g: "CRIME", c: "tv", img: "https://image.tmdb.org/t/p/w300/reKs8PRmZ9Yv7pT9yW6Wp.jpg", s: 5, e: 10 },
-    { id: "1402", t: "The Boys", y: "2019", g: "ACTION", c: "tv", img: "https://image.tmdb.org/t/p/w300/7Y3Hs6p2vKvl6SodpCH97699t.jpg", s: 4, e: 8 },
-    { id: "93405", t: "Squid Game", y: "2021", g: "DRAMA", c: "tv", img: "https://image.tmdb.org/t/p/w300/d99X2M76p2Yv.jpg", s: 1, e: 9 },
-    { id: "82856", t: "The Mandalorian", y: "2019", g: "SCI-FI", c: "tv", img: "https://image.tmdb.org/t/p/w300/sWgBv7LV7psS94IZpBv0srthvS8.jpg", s: 3, e: 8 },
-    { id: "110316", t: "Alice in Borderland", y: "2020", g: "SCI-FI", c: "tv", img: "https://image.tmdb.org/t/p/w300/20mC77hMc7Ej1MvthkgYh97699t.jpg", s: 2, e: 8 },
-    { id: "60625", t: "Rick and Morty", y: "2013", g: "SCI-FI", c: "tv", img: "https://image.tmdb.org/t/p/w300/cvChGrcc8996Re6S9vDUs9GKV3.jpg", s: 7, e: 10 },
-    { id: "76479", t: "The Boys", y: "2019", g: "ACTION", c: "tv", img: "https://image.tmdb.org/t/p/w300/mY7Sj9i9QI9In9I9QI9In9I9QI9.jpg", s: 4, e: 8 },
-    // ... Add more series following this exact format until you reach 50
+    // --- SERIES ---
+    { id: "66732", t: "Stranger Things", y: "2016", g: "SCI-FI", c: "tv", img: "https://image.tmdb.org/t/p/w500/x2LSRm21uTEx8P9uS4NiYpuzpYm.jpg", s: 4, e: 8 },
+    { id: "1396", t: "Breaking Bad", y: "2008", g: "CRIME", c: "tv", img: "https://image.tmdb.org/t/p/w500/ztkUQ09o9h969Re6S9vDUs9GKV3.jpg", s: 5, e: 13 },
+    { id: "71446", t: "Money Heist", y: "2017", g: "CRIME", c: "tv", img: "https://image.tmdb.org/t/p/w500/reKs8PRmZ9Yv7pT9yW6Wp.jpg", s: 5, e: 10 },
+    { id: "1402", t: "The Walking Dead", y: "2010", g: "HORROR", c: "tv", img: "https://image.tmdb.org/t/p/w500/n771f8Iq67p6fXv6p5L8fG5K6.jpg", s: 11, e: 24 },
+    { id: "82856", t: "The Mandalorian", y: "2019", g: "SCI-FI", c: "tv", img: "https://image.tmdb.org/t/p/w500/eU1i6eHXp2klv0tKWpjo7uqcM0N.jpg", s: 3, e: 8 },
 
-    // --- MOVIES SECTION (100 ENTRIES) ---
-    { id: "299534", t: "Avengers Endgame", y: "2019", g: "ACTION", c: "mov", img: "https://image.tmdb.org/t/p/w300/or06vSqzWBFscv1qCG76uC1fsR6.jpg" },
-    { id: "533535", t: "Deadpool & Wolverine", y: "2024", g: "ACTION", c: "mov", img: "https://image.tmdb.org/t/p/w300/h6S6Yn6TrCD0t9_693134.jpg" },
-    { id: "155", t: "The Dark Knight", y: "2008", g: "CRIME", c: "mov", img: "https://image.tmdb.org/t/p/w300/qJ2tW6WMUDp9QEQbvN9pYFWZ9S7.jpg" },
-    { id: "27205", t: "Inception", y: "2010", g: "SCI-FI", c: "mov", img: "https://image.tmdb.org/t/p/w300/edv5CZvRjS99ayO76Y9Z9D9p2.jpg" },
-    { id: "157336", t: "Interstellar", y: "2014", g: "SCI-FI", c: "mov", img: "https://image.tmdb.org/t/p/w300/gEU2QniE6E07Qv8uIUglvYvBvU3.jpg" },
-    { id: "475557", t: "Joker", y: "2019", g: "DRAMA", c: "mov", img: "https://image.tmdb.org/t/p/w300/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg" },
-    { id: "1022789", t: "Inside Out 2", y: "2024", g: "DRAMA", c: "mov", img: "https://image.tmdb.org/t/p/w300/vpnVM9B6NMmAnp6p9S8fG5K6.jpg" },
-    { id: "653346", t: "Kingdom of the Planet of the Apes", y: "2024", g: "SCI-FI", c: "mov", img: "https://image.tmdb.org/t/p/w300/gKkl37S9vDUs9GKV3.jpg" },
-    { id: "823464", t: "Godzilla x Kong", y: "2024", g: "ACTION", c: "mov", img: "https://image.tmdb.org/t/p/w300/tM9Sj9i9QI9In9I9QI9In9I9QI9.jpg" },
-    { id: "693134", t: "Dune: Part Two", y: "2024", g: "SCI-FI", c: "mov", img: "https://image.tmdb.org/t/p/w300/8b8R8Lbd9uS94IZpBv0srthvS8.jpg" }
-    // ... Add more movies following this exact format until you reach 100
+    // --- MOVIES ---
+    { id: "299534", t: "Avengers Endgame", y: "2019", g: "ACTION", c: "mov", img: "https://image.tmdb.org/t/p/w500/or06vSqzWBFscv1qCG76uC1fsR6.jpg" },
+    { id: "533535", t: "Deadpool & Wolverine", y: "2024", g: "ACTION", c: "mov", img: "https://image.tmdb.org/t/p/w500/8cdcl37S9vDUs9GKV3.jpg" },
+    { id: "155", t: "The Dark Knight", y: "2008", g: "CRIME", c: "mov", img: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDp9QEQbvN9pYFWZ9S7.jpg" },
+    { id: "27205", t: "Inception", y: "2010", g: "SCI-FI", c: "mov", img: "https://image.tmdb.org/t/p/w500/edv5CZvRjS99ayO76Y9Z9D9p2.jpg" },
+    { id: "475557", t: "Joker", y: "2019", g: "DRAMA", c: "mov", img: "https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg" }
 ];
 
-// Titan Stability Padding for db.js
-for(let i=0; i<1000; i++) { /* System Integrity - Felix Nyoka V68 */ }
+// Titan Stability Padding - Felix Nyoka V68
+for(let i=0; i<1000; i++) { /* System Integrity */ }
